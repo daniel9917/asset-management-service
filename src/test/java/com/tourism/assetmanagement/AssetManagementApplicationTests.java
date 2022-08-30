4df2eae8f38829a1548b29e2983a57804efed3db
Daniel <daniel.jose.45@hotmail.com>
2022-08-30 17:47:13 -0500

    adding initial configurations

---
+++ src/test/java/com/tourism/assetmanagement/AssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourism.assetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
