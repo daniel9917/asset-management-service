8431c107cba0bec4429d8dd3676fd9cf021566e5
daniel9917 <daniel.jose.45@hotmail.com>
2022-12-28 10:29:40 -0500

    service-enhancements adding docker image name to pair with docker hub repo, adding usage of env variables at ExternalService, adding pom dependency to fix bug with http

--- src/main/java/com/tourism/assetmanagement/service/ExternalService.java
+++ src/main/java/com/tourism/assetmanagement/service/ExternalService.java
@@ -3,6 +3,8 @@ package com.tourism.assetmanagement.service;
 import com.tourism.assetmanagement.model.FormDataDTO;
 import com.tourism.assetmanagement.model.impact.MaturityDTO;
 import com.tourism.errors.ServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -12,13 +14,15 @@ import java.util.List;
 @Service
 public class ExternalService {
 
+    @Autowired
+    private Environment environment;
     private RestTemplate restTemplate;
 
     public ExternalService (){
         restTemplate = new RestTemplate();
     }
     public FormDataDTO getAssetImpact(FormDataDTO requestObject) {
-        String impactURL = "http://localhost:8081/host/impact";
+        String impactURL = environment.getProperty("IMPACT_URL") + "/host/impact" ;
         try {
             MaturityDTO assetMaturity = restTemplate.postForObject(impactURL, requestObject, MaturityDTO.class);
             return FormDataDTO.builder().objectName("maturity").values(List.of(assetMaturity)).build();
